5eaa5e36cb98a912c9191b8ee301a194896643e0
Bilalgithub009 <m.bilaljaliyawala@gmail.com>
2024-05-15 23:25:19 +0500

    bulb on off
    code push

---
+++ app.js
@@ -0,0 +1,31 @@
+
+document.getElementById("img").src = "https://www.shutterstock.com/image-illustration/3d-illustration-light-bulb-isolated-600nw-749587456.jpg"
+document.getElementById("text").innerHTML="off"   
+function btnon(){
+document.getElementById("img").src = "https://www.freeiconspng.com/thumbs/lightbulb-png/light-bulb-png-bulb-png1247-12.png"
+document.getElementById("text").innerHTML= "on"
+document.getElementById("on").style.backgroundColor="rgb(245, 188, 0)"
+document.getElementById("on").style.color= "white"
+}
+
+function btnoff(){
+    document.getElementById("img").src = "https://www.shutterstock.com/image-illustration/3d-illustration-light-bulb-isolated-600nw-749587456.jpg"
+    document.getElementById("text").innerHTML= "off"
+    document.getElementById("off").style.backgroundColor="rgb(245, 188, 0)"
+    document.getElementById("off").style.color= "white"
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
